9a7e700be089559f14dae1b309132d70a51e794a
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-11-29 15:54:30 +0300

    Generating kubernetes secret with consul template

---
+++ scripts/tools/apache-k8s.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+kubectl delete secret apache-ui || echo "Secrets apache-ui not found"
+kubectl create secret tls apache-ui --cert=$(pwd)/scripts/config/apache-k8s/certs/server.crt --key=$(pwd)/scripts/config/apache-k8s/certs/server.key
--- scripts/tools/apache.sh
+++ scripts/tools/apache.sh
@@ -3,7 +3,6 @@ PORT=$1
 cont=vault-apache
 
 docker rm -f $cont || echo "Container $cont doesn't exists"
-docker run --name $cont -v $(pwd)/scripts/config/nginx/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
-    -v $(pwd)/scripts/config/apache/certs:/opt/bitnami/apache/certs \
+docker run --name $cont -v $(pwd)/scripts/config/apache/certs:/opt/bitnami/apache/certs \
     -p ${PORT}:8443 -d  bitnami/apache:latest
 echo "Apache start check URL https://localhost:${PORT}"
