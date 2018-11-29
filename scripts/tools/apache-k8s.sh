6e34513d640db6ba0e7476becb8ca716f0c8a327
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-11-29 15:57:40 +0300

    Generating kubernetes secret with consul template

--- scripts/tools/apache-k8s.sh
+++ scripts/tools/apache-k8s.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 kubectl delete secret apache-ui || echo "Secrets apache-ui not found"
 kubectl create secret tls apache-ui --cert=$(pwd)/scripts/config/apache-k8s/certs/server.crt --key=$(pwd)/scripts/config/apache-k8s/certs/server.key
+rm -f $(pwd)/scripts/config/apache-k8s/certs/server.key
