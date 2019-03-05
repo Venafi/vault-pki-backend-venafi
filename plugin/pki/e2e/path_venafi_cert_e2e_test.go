48fa6d73fae085b6bef6924ebd5558fbaf823df8
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2019-03-05 18:36:38 +0300

    increase start timeout

--- plugin/pki/e2e/path_venafi_cert_e2e_test.go
+++ plugin/pki/e2e/path_venafi_cert_e2e_test.go
@@ -17,7 +17,7 @@ func init() {
 	commandWithArgs("docker-compose", "down", "--remove-orphans")
 	commandWithArgs("docker", "images")
 	commandWithArgs("docker-compose", "up", "-d")
-	time.Sleep(10)
+	time.Sleep(20)
 
 	init := run(fmt.Sprintf("docker exec %s vault operator init -key-shares=1 -key-threshold=1", vaultContainerName))
 
